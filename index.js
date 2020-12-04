06235cf99b06e55825f358f4911a66714ae4fb4b
BAD MOTHERFUCKER <lassestausgaard@gmail.com>
2020-12-04 14:31:55 +0100

    Mobile UI + socket connection
    swiping + tapping triggers keyboard events

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const path = require('path');
 
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http)
+const io = require('socket.io')(http);
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.
 
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
     var list = ["item1", "item2", "item3"];
     res.json(list);
     console.log('Sent list of items');
+});
+app.get('/phone', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/phone.html'));
 });
 
 // Handles any requests that don't match the ones above
@@ -22,13 +27,19 @@ app.get('*', (req,res) =>{
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('navigation', (input) => {
+        console.log('user action: ' + input);
+        io.sockets.emit('navigation', input);
+    })
+
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
 });
-app.get('/phone:*', (req, res)=>{
-    res.sendFile(path.join(__dirname+'/phone.html'))
-});
+
+
+
 const port = process.env.PORT || 5000;
 // app.listen(port);
 http.listen(port, ()=>{
