997cda7fbb076ef370db69727131eac77e841160
Frederik Moroder <frotti92@hotmail.com>
2020-12-16 18:13:17 +0100

    test v3

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/phone.css', (req, res) => {
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'./client/build/index.html'));
 });
 
 io.on('connection', (socket) => {
